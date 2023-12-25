affae368ad0ac3c17a963aa653a6955f4598d20c
Code-Abhijit-Salunkhe <78486237+Code-Abhijit-Salunkhe@users.noreply.github.com>
2023-12-25 21:54:53 +0000

    test dummy commit and push

--- src/main/java/com/dsandalgo/leetcode/easy/TwoSum.java
+++ src/main/java/com/dsandalgo/leetcode/easy/TwoSum.java
@@ -1,36 +1,35 @@
 package com.dsandalgo.leetcode.easy;
 
-/**
- * @Author : Abhijit on 25/12/2023
- */
-
 import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice. *
  * You can return the answer in any order.
- *
  * Example 1: *
  * Input: nums = [2,7,11,15], target = 9
  * Output: [0,1]
  * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- *
  * Example 2: *
  * Input: nums = [3,2,4], target = 6
  * Output: [1,2]
- *
  * Example 3: *
  * Input: nums = [3,3], target = 6
  * Output: [0,1]
  */
 public class TwoSum {
+    private TwoSum(){
+        throw new IllegalStateException("Utility Class");
+    }
+
     /**
      * Brute force
      * Time Complexity : B(O) = n square
+     *
      */
-    class Solution {
+    public static class Solution {
         public int[] twoSum(int[] nums, int target) {
             int[] result = new int[2];
             for(int i = 0; i < nums.length - 1; i++)
@@ -48,7 +47,8 @@ public class TwoSum {
     /**
      *  Using HashMap and Complement - FAST Solution
      */
-    class Solution1 {
+    public static class Solution1 {
+
         public int[] twoSum(int[] nums, int target) {
             int[] result = new int[2];
             Map<Integer, Integer> complementMap = new HashMap<>();
